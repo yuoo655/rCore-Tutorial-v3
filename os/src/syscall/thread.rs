4f4c8d234f5bc54043ff3aa23d449bf4fa0e4244
yuoo655 <lenuulan@gmail.com>
2022-05-11 15:05:23 +0800

    fix sys_waittid (always return -1 before)

--- os/src/syscall/thread.rs
+++ os/src/syscall/thread.rs
@@ -37,45 +37,33 @@ pub fn sys_gettid() -> isize {
 /// thread does not exist, return -1
 /// thread has not exited yet, return -2
 /// otherwise, return thread's exit code
-pub fn sys_waittid(tid: usize) -> i32 {
+pub fn sys_waittid(pid: usize) -> i32 {
 
-    let task = current_task().unwrap();
-
-
-    let tgid = task.tgid;
-
-    // parent pid
-    let pid = task.pid.0;
+    let waited_task = pid2task(pid);
 
 
-    println!("wait pid:{}, current: pid={} tgid={}", tid,pid,tgid);
+    if let Some(task) = waited_task {
+        let pid = task.pid.0;
+        let tgid = task.tgid;
 
-    let waited_task = pid2task(tid);
-
-    // a thread cannot wait for itself
-    if pid == tgid {
-        return -1;
-    }
-
-    
-    let mut exit_code: Option<i32> = None;
-
-    if let Some(waited_task) = waited_task {
-        
-        if let Some(waited_exit_code) = waited_task.inner_exclusive_access().get_exit_code(){
-            exit_code = Some(waited_exit_code);
+        if pid == tgid {
+            // main thread cannot be waited
+            return -1;
         }
-    } else {
-        // waited thread does not exist
-        return -1;
-    }
-    if let Some(exit_code) = exit_code {
 
-        remove_from_pid2task(tid);
-        exit_code
-    } else {
-        // waited thread has not exited
-        -2
+        if let Some(waited_exit_code)  = task.inner_exclusive_access().exit_code {
+
+            // thread has exited
+            return waited_exit_code;
+
+        }else {
+            // thread has not exited yet
+            -2 
+        }
+
+    }else{
+        // cannot find thread
+        -1
     }
 
 }
